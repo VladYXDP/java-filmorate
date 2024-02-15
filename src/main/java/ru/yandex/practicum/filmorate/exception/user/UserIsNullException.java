55c1c9fe632ef37118d86ca2366c61cb439ef826
Vlad Lysenko <me2ur.cat@yandex.ru>
2024-02-15 22:40:06 +0300

    Выполнение тестов

--- src/main/java/ru/yandex/practicum/filmorate/exception/user/UserIsNullException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/user/UserIsNullException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception.user;
 
-public class UserIsNullException extends RuntimeException{
+public class UserIsNullException extends RuntimeException {
 
     public UserIsNullException(String message) {
         super(message);
