cd8764dd2f9e00587303d46cb4002a5df88c23b6
Vlad Lysenko <me2ur.cat@yandex.ru>
2024-06-12 20:15:41 +0300

    Прохождение проверок гита

--- src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
@@ -6,12 +6,20 @@ import java.util.Set;
 
 public interface ReviewStorage {
     Review create(Review review);
+
     Review update(Review review);
+
     void delete(long id);
+
     Review get(long id);
+
     Set<Review> getAllById(long filmId, long count);
+
     void like(long id, long userId);
+
     void dislike(long id, long userId);
+
     void deleteLike(long id, long userId);
+
     void deleteDislike(long id, long userId);
 }
